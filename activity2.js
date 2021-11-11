6f288c6bb6012e6a052b7448a54064de714f46f0
ilikeHippos17 <91993463+ilikeHippos17@users.noreply.github.com>
2021-11-11 17:40:03 -0500

    Add files via upload

---
+++ activity2.js
@@ -0,0 +1,12 @@
+function updateScore()
+{
+    score = score + 1;
+    document.getElementById("score").innerHTML = "Score: " + score;
+}
+function saveScore()
+{
+    localStorage.setItem("score", score);
+}
+function nextPage() {
+    window.location = "activity-2.html";
+}
